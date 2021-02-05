2a5e9e50c82014796f69f7d5d5c77e7871474d73
Daniel Martínez Salinas <dmartinez@rhomb.io>
2021-02-05 14:13:31 +0100

    Added funcionailty for ADC 0-3.3V range

--- ACS712.cpp
+++ ACS712.cpp
@@ -13,21 +13,23 @@
 // 0.1.4    2020-08-02 Allow for faster processors
 // 0.2.0    2020-08-02 Add autoMidPoint
 // 0.2.1    2020-12-06 Add arduino-CI + readme + unittest + refactor
+// 0.2.1    2021-02-05 Add mA_DC option for ADC 0-3.3V range. Daniel Martínez
 
 #include "ACS712.h"
 
-ACS712::ACS712(uint8_t analogPin, float volts, uint16_t maxADC, uint8_t mVperA)
-{
+ACS712::ACS712(uint8_t analogPin, float volts, uint16_t maxADC, uint8_t mVperA,
+               bool isADC3_3V) {
+  _isADC3_3V = isADC3_3V;
   _pin = analogPin;
-  _mVpstep     = 1000.0 * volts / maxADC;  // 1x 1000 for V -> mV
+  _mVpstep = 1000.0 * volts / maxADC;  // 1x 1000 for V -> mV
   _mVperAmpere = mVperA;
-  _formFactor  = 0.70710678119;  // 0.5 * sqrt(2);  TODO: should be smaller in practice 0.5 ?
-  _midPoint    = maxADC / 2;
-  _noisemV     = 21;             // Noise is 21mV according to datasheet
+  _formFactor = 0.70710678119;  // 0.5 * sqrt(2);  TODO: should be smaller in
+                                // practice 0.5 ?
+  _midPoint = maxADC / 2;
+  _noisemV = 21;  // Noise is 21mV according to datasheet
 }
 
-int ACS712::mA_AC(uint8_t freq)
-{
+int ACS712::mA_AC(uint8_t freq) {
   uint32_t start = micros();
   uint16_t period = ((freq == 60) ? 16670 : 20000);
   uint16_t samples = 0;
@@ -42,41 +44,44 @@ int ACS712::mA_AC(uint8_t freq)
     int val = analogRead(_pin);
     if (val < _min) _min = val;
     if (val > _max) _max = val;
-    if (abs(val - _midPoint) <= (_noisemV/_mVpstep)) zeros++;
+    if (abs(val - _midPoint) <= (_noisemV / _mVpstep)) zeros++;
   }
   int point2point = (_max - _min);
 
   // automatic determine _formFactor / crest factor
   float D = 0;
   float FF = 0;
-  if (zeros > samples * 0.025)
-  {
-    D = 1.0 - (1.0 * zeros) / samples;    // % SAMPLES NONE ZERO
-    FF = sqrt(D) * 0.5 * sqrt(2);         // ASSUME NON ZERO PART ~ SINUS
-  }
-  else  // # zeros is small => D --> 1 --> sqrt(D) --> 1
+  if (zeros > samples * 0.025) {
+    D = 1.0 - (1.0 * zeros) / samples;  // % SAMPLES NONE ZERO
+    FF = sqrt(D) * 0.5 * sqrt(2);       // ASSUME NON ZERO PART ~ SINUS
+  } else  // # zeros is small => D --> 1 --> sqrt(D) --> 1
   {
     FF = 0.5 * sqrt(2);
   }
   _formFactor = FF;
 
-  // math could be partially precalculated: C = 1000.0 * 0.5 * _mVpstep / _mVperAmpere;
-  // rounding?
+  // math could be partially precalculated: C = 1000.0 * 0.5 * _mVpstep /
+  // _mVperAmpere; rounding?
   return 1000.0 * 0.5 * point2point * _mVpstep * _formFactor / _mVperAmpere;
 }
 
-int ACS712::mA_DC()
-{
-    // read twice to stabilize...
-    analogRead(_pin);
+int ACS712::mA_DC() {
+  // read twice to stabilize...
+  // read twice to stabilize...
+  // Option A: 0-5V range ADC.
+  if (_isADC3_3V) {
+    return 1000 * (analogRead(_pin) * _mVpstep - 2500) / _mVperAmpere;
+  }
+  // Option B: 0-3.3V range ADC
+  else {
     int steps = analogRead(_pin) - _midPoint;
     return 1000.0 * steps * _mVpstep / _mVperAmpere;
+  }
 }
 
 // configure by sampling for 2 cycles of AC
 // Also works for DC as long as no current flowing
-void ACS712::autoMidPoint(uint8_t freq)
-{
+void ACS712::autoMidPoint(uint8_t freq) {
   uint32_t start = micros();
   uint16_t twoPeriods = ((freq == 60) ? 16670 : 20000) * 2;
   uint32_t total = 0;
@@ -84,8 +89,9 @@ void ACS712::autoMidPoint(uint8_t freq)
   while (micros() - start < twoPeriods) {
     uint16_t reading = analogRead(_pin);
     total += reading;
-    samples ++;
-    // Delaying ensures we won't overflow since we'll perform a maximum of 40,000 reads
+    samples++;
+    // Delaying ensures we won't overflow since we'll perform a maximum of
+    // 40,000 reads
     delayMicroseconds(1);
   }
   _midPoint = total / samples;
